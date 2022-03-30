672e735c1f0d58e378403d92b3913cced52c450b
nittanando <sarkar.anando2995@gmail.com>
2022-03-30 17:58:29 +0600

    dynamically update data on 2nd push,30th march

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -1,33 +1,42 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 299,
+    date: new Date(2022, 7, 20),
+  },
+  { id: "e2", title: "House Rent", amount: 200, date: new Date(2020, 4, 1) },
+  {
+    id: "e3",
+    title: "Medicine Cost",
+    amount: 100,
+    date: new Date(2020, 3, 23),
+  },
+  {
+    id: "e5",
+    title: "Furniture Cost",
+    amount: 300,
+    date: new Date(2019, 9, 23),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 299,
-      date: new Date(2020, 7, 20),
-    },
-    { id: "e2", title: "House Rent", amount: 200, date: new Date(2020, 4, 1) },
-    {
-      id: "e3",
-      title: "Medicine Cost",
-      amount: 100,
-      date: new Date(2020, 3, 23),
-    },
-    {
-      id: "e5",
-      title: "Furniture Cost",
-      amount: 300,
-      date: new Date(2020, 9, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(Dummy_Expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div className="App">
       <h2>Expense Tracker Application</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
