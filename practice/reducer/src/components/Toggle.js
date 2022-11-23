106cd4fcc9358365a9b5ad27272fc36dfddf1e73
Nittanando <sarkar.anando2995@gmail.com>
2022-11-23 20:27:17 +0600

    jobor

--- practice/reducer/src/components/Toggle.js
+++ practice/reducer/src/components/Toggle.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 function Toggle() {
+  const [state, dispatch] = useReducer(reducer, { toggle: "" });
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "toggle":
+        return { toggle: state.toggle };
+    }
+  }
+
   return (
     <div className="toggle-main">
       <div className="toggle-sub">
