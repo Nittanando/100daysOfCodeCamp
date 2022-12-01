e6add5924e1feedf3cae9b6371a2cbcc4066695d
Nittanando <sarkar.anando2995@gmail.com>
2022-12-02 01:52:33 +0600

    react bootstrap

--- practice/reducer/src/App.js
+++ practice/reducer/src/App.js
@@ -1,12 +1,52 @@
-import './App.css';
-import Countreducer from './components/Countreducer';
-import Toggle from './components/Toggle';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Countreducer/>
-      <Toggle/>
+    <div className="container-fluid g-0">
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="d-flex align-items-center justify-content-between w-100 ps-lg-4 pe-lg-4 ps-md-3 pe-md-3 ps-sm-2 pe-sm-2 pe-1 ps-1">
+          <span className="navbar-brand fs-1">
+            Logo<strong className="text-danger"> .</strong>
+          </span>
+
+          <span>
+            <button
+              className="p-0 bg-transparent border-0"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasRight"
+              aria-controls="offcanvasRight"
+            >
+              <i className="bi bi-menu-button-wide-fill text-light fs-3"></i>
+            </button>
+
+            <div
+              className="offcanvas bg-dark text-light offcanvas-end"
+              tabIndex="-1"
+              id="offcanvasRight"
+              aria-labelledby="offcanvasRightLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+                <button
+                  type="button"
+                  className="btn-close text-reset bg-light"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="w-100">
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+            </div>
+          </span>
+        </div>
+      </nav>
     </div>
   );
 }
--- practice/reducer/src/index.js
+++ practice/reducer/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
